00495c96c94668152993e00d867142c83c66fb0e
Andrew Mason <mason@case.edu>
2015-10-09 13:39:30 -0400

    fix bad ln option and git clone vundle if it's not there

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
-# vim
 wd=$(pwd -P)
-ln -fhs $wd/vim/.vim $HOME/.vim
-ln -fhs $wd/vim/.vimrc $HOME/.vimrc
+
+# vim
+ln -fs $wd/vim/.vim $HOME/.vim
+if [ !-d $HOME/.vim/bundle/Vundle.vim ]
+  # give the people vundle if they don't have it
+  git clone git@github.com:gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
+ln -fs $wd/vim/.vimrc $HOME/.vimrc
 vim +PluginInstall +qall
 
 # bash
-ln -fhs $wd/bash/.bashrc $HOME/.bashrc
+ln -fs $wd/bash/.bashrc $HOME/.bashrc
 if [[ $(uname) = 'Darwin' ]]; then
   echo 'symlinked bash/.bashrc to $HOME/.bashrc'
   echo 'you should probably have your .bash_profile source $HOME/.bashrc, or symlink .bash_profile yourself'
