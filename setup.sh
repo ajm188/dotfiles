a06de98609ee64291c2df3262a253c736331c16e
Andrew Mason <mason@case.edu>
2015-11-24 15:40:41 -0500

    add yesno for everything, add fish

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wd=$(pwd -P)
 
 # https://stackoverflow.com/questions/226703/how-do-i-prompt-for-input-in-a-linux-shell-script
@@ -12,13 +13,15 @@ yesno () {
 }
 
 # vim
-ln -fs $wd/.vim $HOME/.vim
-if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
-  # give the people vundle if they don't have it
-  git clone git@github.com:gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+if yesno "Do you want to install .vimrc + .vim?"; then
+    ln -fs $wd/.vim $HOME/.vim
+    if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+      # give the people vundle if they don't have it
+          git clone git@github.com:gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    fi
+    ln -fs $wd/.vimrc $HOME/.vimrc
+    vim +PluginInstall +qall
 fi
-ln -fs $wd/.vimrc $HOME/.vimrc
-vim +PluginInstall +qall
 
 # bash
 if yesno "Do you want to install .bashrc?"; then
@@ -27,4 +30,9 @@ if yesno "Do you want to install .bashrc?"; then
         echo 'symlinked .bashrc to $HOME/.bashrc'
         echo 'you should probably have your .bash_profile source $HOME/.bashrc, or symlink .bash_profile yourself'
     fi
+fi
+
+if yesno "Do you want to install .config/fish?"; then
+    mkdir -p $HOME/.config
+    ln -fs $wd/.config/fish $HOME/.config/fish
 fi
