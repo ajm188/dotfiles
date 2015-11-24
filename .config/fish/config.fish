84139ca1455d813379b3084522c6aeeecbd712dd
Andrew Mason <mason@case.edu>
2015-11-23 23:09:29 -0500

    set vim to be the default editor in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
 if ls /opt/local/bin >/dev/null ^/dev/null
     set -x PATH /opt/local/bin $PATH
 end
+
+# This doesn't actually have to be here, but since I'm sharing my config,
+# I'll leave it here.
+set -U EDITOR vim
