ca8a381f8c078ac638729750b88d4e47b6d8ffa6
Andrew Mason <mason@case.edu>
2015-11-30 13:41:35 -0500

    set up some PATH stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,13 @@
-if ls /opt/local/bin >/dev/null ^/dev/null
-    set -x PATH /opt/local/bin $PATH
+if test -e /opt/local/
+    set -l port_prefix /opt/local
+    set -x PATH $port_prefix/bin $port_prefix/sbin $PATH
+    # This currently breaks man for non macports stuff, since $MANPATH is
+    # empty? idk...
+    # set -x MANPATH $port_prefix/share/man $MANPATH
+end
+
+if test -e $HOME/bin/
+    set -x PATH $HOME/bin $PATH
 end
 
 # This doesn't actually have to be here, but since I'm sharing my config,
