8707b3142d7fba206056b6b0c7861c62b08b337b
Andrew Mason <mason@case.edu>
2015-12-01 22:29:43 -0500

    fix right prompt bug. also just use __ functions

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,14 +1,15 @@
+function __fish_right_prompt_wrap_time
+    echo '['; set_color yellow; echo $argv; set_color normal; echo ']'
+end
+
 function fish_right_prompt
     set -l head (git rev-parse --abbrev-ref HEAD ^/dev/null)
-    function wrap_time
-        echo '['; set_color yellow; echo $argv; set_color normal; echo ']'
-        functions -e wrap_git_time
+    set -l cur_time (date +%H:%M:%S)
+    if test -n "$head"
+        set -l gtime (git log -1 --pretty=format:"%ar" ^/dev/null)
+        if test -n "$gtime"
+            set cur_time $gtime
+        end
     end
-
-    if count $head >/dev/null
-        set -l gtime (git log -1 --pretty=format:"%ar")
-        wrap_time $gtime
-    else
-        wrap_time (date +%H:%M:%S)
-    end
+    __fish_right_prompt_wrap_time $cur_time
 end
