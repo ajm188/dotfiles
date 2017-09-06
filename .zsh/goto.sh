d0cc2069cd347233675eea1589e7750db8d52506
Andrew Mason <mason@yelp.com>
2017-09-06 08:21:15 -0700

    Bugfixes and it's working
    1. Checking if the session exists needs double brackets for the conditional, as
    well as piping stderr to dev/null. `tmux` uses stderr to complain if it can't
    find the session. Based on the man page, there appears to be no way to turn
    this off.
    2. The check on the GOTO_PROJECT_DIR variable needs to be short-circuited with
    an &&, not an ||, so that HOME is used as the default only if GOTO_PROJECT_DIR
    is empty. In the *very* strange case that you want to put all your projects in
    "/", you should explicitly set that.
    
    TODO: completions

--- .zsh/goto.sh
+++ .zsh/goto.sh
@@ -1,14 +1,14 @@
 function goto() {
     local project="$1"
 
-    [ tmux has -t $project ] || _tmux_create $project
+    [[ $(tmux has -t $project 2>/dev/null) ]] || _tmux_create $project
     _tmux_attach $project
 }
 
 function _tmux_create() {
     local project="$1"
     local base=$GOTO_PROJECT_DIR
-    [ -n $GOTO_PROJECT_DIR ] && local base=$HOME
+    [ -n $GOTO_PROJECT_DIR ] || local base=$HOME
 
     local workdir="$base/$project"
     tmux new -d -s $project -c $workdir
