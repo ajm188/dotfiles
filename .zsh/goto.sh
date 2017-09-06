7214e371d43fa5242cee8f0390e0024013124eb2
Andrew Mason <andrew@fixedpoint.xyz>
2017-09-05 18:33:23 -0700

    Add WIP goto script, still untested

---
+++ .zsh/goto.sh
@@ -0,0 +1,25 @@
+function goto() {
+    local project="$1"
+
+    [ tmux has -t $project ] || _tmux_create $project
+    _tmux_attach $project
+}
+
+function _tmux_create() {
+    local project="$1"
+    local base=$GOTO_PROJECT_DIR
+    [ -n $GOTO_PROJECT_DIR ] && local base=$HOME
+
+    local workdir="$base/$project"
+    tmux new -d -s $project -c $workdir
+}
+
+function _tmux_attach() {
+    local project="$1"
+
+    if [ -n $TMUX ]; then
+        tmux switch -t $project
+    else
+        tmux attach -t $project
+    fi
+}
